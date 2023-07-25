3cba143df60b2afae0ca20525ff85cdf8b8b9087
Ndaru-Senopati-Nusa <140544533+Ndaru-Senopati-Nusa@users.noreply.github.com>
2023-07-26 00:57:34 +0700

    Update FormController.php

--- app/Controllers/FormController.php
+++ app/Controllers/FormController.php
@@ -1,1 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class FormController extends BaseController
+{
+    public function profile()
+    {
+        return view('layout/profile');
+    }
+    public function experience()
+    {
+        return view('layout/experience');
+    }
+}
 
